461641ca96debee34c77df2407563706826ddc03
kehdcode <kehindedawood@gmail.com>
2020-04-08 16:16:36 +0100

    more on menu components

--- my-food/src/App.js
+++ my-food/src/App.js
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  NavbarText
-} from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, NavbarText } from 'reactstrap';
+import Menu from './Components/Menu'
 
 const App = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,6 +26,8 @@ const App = (props) => {
           <NavbarText>My Orders</NavbarText>
         </Collapse>
       </Navbar>
+
+      <Menu />
       </div>
   
   );
--- my-food/src/Components/Menu.js
+++ my-food/src/Components/Menu.js
@@ -1,70 +1,122 @@
 import React, { Component } from 'react'
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import {Media} from 'reactstrap';
 
 
 
 class Menu extends Component {
     constructor(props){
         super(props)
-            this.state ={
-                dishes: [
-                    {
-                        id: 0,
-                        name: 'Uthhappizza',
-                        image: 'assests/images/food 1.jpg',
-                        category: 'mains',
-                        label: 'hot',
-                        price: '4.99',
-                        description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
-                        
-                    },
-                    {
-                        id: 1,
-                        name: 'Wamilothpo',
-                        image: 'assests/images/food 2.jpg',
-                        category: 'desert',
-                        label: 'hot',
-                        price: '5.39',
-                        description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
-                        
-                    },
-                    {
-                        id: 2,
-                        name: 'Ottawamilo',
-                        image: 'assests/images/food 3.jpg',
-                        category: 'mains',
-                        label: 'hot',
-                        price: '2.99',
-                        description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
-                        
-                    },
-                    {
-                        id: 3,
-                        name: 'Uthhappizza',
-                        image: 'assests/images/food 4.jpg',
-                        category: 'mains',
-                        label: 'hot',
-                        price: '4.99',
-                        description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
-                        
-                    },
-                    {
-                        id: 4,
-                        name: 'Uthhappizza',
-                        image: 'assests/images/food 5.jpg',
-                        category: 'appetizzer',
-                        label: 'hot',
-                        price: '6.99',
-                        description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
-                        
-                    }
-                ]
-            
+
+        this.state = {
+            dishes: [
+                {
+                    id: 0,
+                    name: 'Uthhappizza',
+                    image: '/assests/images/food 1.jpg',
+                    category: 'mains',
+                    label: 'hot',
+                    price: '4.99',
+                    description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
+                    
+                },
+                {
+                    id: 1,
+                    name: 'Wamilothpo',
+                    image: '/assests/images/food 2.jpg',
+                    category: 'desert',
+                    label: 'hot',
+                    price: '5.39',
+                    description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
+                    
+                },
+                {
+                    id: 2,
+                    name: 'Ottawamilo',
+                    image: '/assests/images/food 3.jpg',
+                    category: 'mains',
+                    label: 'hot',
+                    price: '2.99',
+                    description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
+                    
+                },
+                {
+                    id: 3,
+                    name: 'Uthhappizza',
+                    image: '/assests/images/food 4.jpg',
+                    category: 'mains',
+                    label: 'hot',
+                    price: '4.99',
+                    description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
+                    
+                },
+                {
+                    id: 4,
+                    name: 'Uthhappizza',
+                    image: '/assests/images/food 5.jpg',
+                    category: 'appetizzer',
+                    label: 'hot',
+                    price: '6.99',
+                    description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
+                    
+                },
+                {
+                    id: 5,
+                    name: 'Uthhappizza',
+                    image: '/assests/images/food 2.jpg',
+                    category: 'appetizzer',
+                    label: 'hot',
+                    price: '6.99',
+                    description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
+                    
+                },
+                {
+                    id: 6,
+                    name: 'Uthhappizza',
+                    image: '/assests/images/food 3.jpg',
+                    category: 'appetizzer',
+                    label: 'hot',
+                    price: '6.99',
+                    description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
+                    
+                },
+                {
+                    id: 7,
+                    name: 'Uthhappizza',
+                    image: '/assests/images/food 4.jpg',
+                    category: 'appetizzer',
+                    label: 'hot',
+                    price: '6.99',
+                    description: 'Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan.'
+                    
+                }
+
+
+            ]
         }
+
     }
     render(){
-        const menu ='';
+        const menu = this.state.dishes.map((dish) =>{
+            return(
+                <div className="col-md-3" key={dish.id}>
+                <Card>
+                <CardImg top width="100%" src={dish.image} alt={dish.name} />
+                <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+                <CardSubtitle> ${dish.price}</CardSubtitle>
+                <Button className="success">Place Order</Button>
+                </CardBody>
+                </Card>
+                </div>
+            )
+        })
         return(
-            <div>
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
 
             </div>
         )
