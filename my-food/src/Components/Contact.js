d3015877b2412ae7287e96b02af6e0018bf95a49
kehdcode <kehindedawood@gmail.com>
2020-04-14 21:55:50 +0100

    react form and install spring for animation

--- my-food/src/Components/Contact.js
+++ my-food/src/Components/Contact.js
@@ -1,16 +1,63 @@
 import React,  { Component } from "react";
+import {Form, FormGroup, Label, Input, Col, Button, FormText} from 'reactstrap'
 
 
 
 
 class Contact extends Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.state= {
+            firstName: '',
+            lastName: '',
+            EmailAdd: '',
+            comment: '',
+            phoneNum: '',
+            agree: false,
+            contactType: 'Tel.'
+        }
+
     }
 
     render(){
         return(
-            <h1> Contact Us</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                    <h4 className="text-center"> Give Us Feedback</h4>
+                    </div>
+                    <div className="col-12 col-md-9">
+                    <Form>
+      <FormGroup row>
+        <Label for="exampleEmail" sm={2}>Email</Label>
+        <Col sm={10}>
+          <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="examplePassword" sm={2}>Password</Label>
+        <Col sm={10}>
+          <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+        </Col>
+      </FormGroup>
+     
+     
+      <FormGroup row>
+        <Label for="exampleText" sm={2}>Text Area</Label>
+        <Col sm={10}>
+          <Input type="textarea" name="text" id="exampleText" />
+        </Col>
+      </FormGroup>
+      <FormGroup check row>
+        <Col sm={{ size: 10, offset: 2 }}>
+          <Button>Submit</Button>
+        </Col>
+      </FormGroup>
+    </Form>
+                    </div>
+                </div>
+            </div>
+            
         )
     }
 }
